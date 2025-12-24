9091ba632fabe697c6a760ee9a8ffb8c7e3db529
Tekin Bicer <bicer@anl.gov>
2025-12-24 00:28:26 +0000

    Sample reconstruction script using apptainer is added: 03_apptainer_fm4t_test_recon.sh

--- deployment/03_apptainer_fm4t_test_recon.sh
+++ deployment/03_apptainer_fm4t_test_recon.sh
@@ -1,2 +1,3 @@
 apptainer exec --nv --bind /grand/hp-ptycho:/mnt/hp-ptycho apptainer_fm4t.sif \
-  /opt/conda/envs/LDM_Resample/bin/python 
+    uv run --directory='/FM4T' sample_condn_recon.py --diffusion_config='/mnt/hp-ptycho/bicer/FM4T/models/ldm/model.ckpt' \
+    --save_dir='/mnt/hp-ptycho/bicer/FM4T/results/trial_May1_25/CT_recon/sino_samples_256/admm_match_sf1_p1e_4/pg20_gamma_var/gamma1e4'
--- deployment/apptainer_recon_test.sh
+++
@@ -1,3 +0,0 @@
-apptainer exec --nv --bind /grand/hp-ptycho:/mnt/hp-ptycho apptainer_fm4t.sif \
-    uv run --directory='/FM4T' sample_condn_recon.py --diffusion_config='/mnt/hp-ptycho/bicer/FM4T/models/ldm/model.ckpt' \
-    --save_dir='/mnt/hp-ptycho/bicer/FM4T/results/trial_May1_25/CT_recon/sino_samples_256/admm_match_sf1_p1e_4/pg20_gamma_var/gamma1e4'
